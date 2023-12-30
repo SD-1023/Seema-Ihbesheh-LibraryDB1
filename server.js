6360fb5a582ffa801e10339bc31087e3cd200520
SeemaIhbesheh <73760401+Seemaihbesheh@users.noreply.github.com>
2023-12-30 14:23:50 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port = 3001;
+import db from  './app/models';
+db.sequelize.sync()
+    .then(() => {
+    console.log('Connection has been established successfully.');
+})
+    .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+});
+app.get("/", (req, res) => {
+    res.json({ message: "welcom to nodejs app" });
+});
+require("./app/routes/book.routes")(app);
+require("./app/routes/publisher.routes")(app);
+require("./app/routes/comment.routes")(app);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
